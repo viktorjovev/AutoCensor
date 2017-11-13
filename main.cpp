764047a0c302a769ba35c54daccba127c90dc37d
Victor Yovev <VictorYovev@users.noreply.github.com>
2017-11-13 08:47:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,7 +21,9 @@ void LoadBadWords()
 }
 int main()
 {
+   //Loading Bad words from BadWords.txt
    LoadBadWords();
+  //To hide console app
    Stealth();
    char key;
     while(true)
@@ -30,10 +32,12 @@ int main()
         for(key=8; key<=255; key++)
             if(GetAsyncKeyState(key)==-32767)
               {
+                //When the user click SPACE, SHIFT,RETURN,TAB,CAPITAL  string will clear.
                 if(int(key)==VK_SPACE || int(key)==VK_SHIFT || int(key)==VK_RETURN || int(key)==VK_TAB || int(key)==VK_CAPITAL)
                  { ::text.clear(); continue;}
-
+                    //when u click any key on keyboard, the string will increase with CHARS;
                    ::text+=key;
+                     //Every time when u click key on keyboard, the app check string if is found in array string 
                     if(find(BAD_WORDS.begin(),BAD_WORDS.end(),::text)!=BAD_WORDS.end()){
                     MessageBox(HWND_DESKTOP,"WARNING! WARNING! WARNING!\n      DON'T USE BAD WORDS","AntiCensor",MB_SYSTEMMODAL);
                         ::text.clear();
