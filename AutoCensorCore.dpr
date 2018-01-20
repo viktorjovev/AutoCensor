a2b7f917b0b878294466a291fc06f7fae9455d95
Victor Yovev <VictorYovev@users.noreply.github.com>
2018-01-20 03:09:31 +0100

    Add files via upload
    AutoCensor v2.5 =>
    Algorithm improved. (Binary Search)
    Мore Еfficient.
    More added BadWords.

--- AutoCensorCore.dpr
+++ AutoCensorCore.dpr
@@ -14,8 +14,8 @@ uses
   Dialogs;
 
 var text:string;
- var  i :integer;
  var key: Byte;
+ var Index: Integer;
  var badwords:TStringList;
  function GetConsoleWindow: HWND; stdcall; external kernel32;
 
@@ -23,21 +23,20 @@ begin
    ShowWindow(GetConsoleWindow, SW_HIDE);
    badwords:=TStringList.Create;
    badwords.LoadFromFile('BadWords.txt');
+   badwords.Sort;
   while true do begin
      Sleep(10);
-       for key:=8 to 255 do begin
-          if GetAsyncKeyState(key) = -32767 then begin
-          if key=VK_BACK then begin SetLength(text, Length(text) - 1); Continue; end;
-             if (key=VK_SPACE) or (key=VK_SHIFT) or (key=VK_RETURN) or (key=VK_TAB) or (key=VK_CAPITAL) then
-               begin text:=''; Continue; end;
-              text:=text+CHR(key);
-              for i:= 0 to badwords.Count-1 do
-              if text=badwords[i] then  begin
-              MessageDlg('Hey take it easy man, breathe and take that back!!!',mtWarning,[mbOK],0);
+       for key:=48 to 91 do begin
+       if GetAsyncKeyState(ord(VK_BACK))=-32767 then begin SetLength(text, Length(text) - 1); break;  end;
+             if (GetAsyncKeyState(ord(VK_SPACE))<0) or (GetAsyncKeyState(ord(VK_RETURN))<0) or (GetAsyncKeyState(ord(VK_TAB))<0)then begin text:=''; break; end;
+       if GetAsyncKeyState(key) = -32767 then begin
+          text:=text+CHR(key);
+              if badwords.Find(text,Index) then begin
+              MessageDlg(#13+'Hey take it easy man, breathe and take that back!!!',mtWarning,[],0);
               text:='';
-              end;
-              end;
-              end;
-              end;
-
+              break;
+          end;
+       end;
+     end;
+  end;
 end.
